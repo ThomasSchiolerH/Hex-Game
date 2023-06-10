9104c12238915e89df937291611f00f6a1da01a7
adamajane <adamajane@gmail.com>
2023-06-10 22:43:41 +0200

    Added menu items (pvp, p v computer, etc.)

--- menuScreen.py
+++ menuScreen.py
@@ -29,44 +29,79 @@ def show_menu():
 
     surface = pygame.display.set_mode((WIDTH, HEIGHT))
 
-    #Menus
+    # Menus
     def start_game():
+        main_menu._open(game_mode_menu)
+
+    """
+    def level_menu():
+        main_menu._open(game_mode_menu)
+    """
+
+    def player_vs_player_game():
         game = Game(screen)
         game.play()
 
-    def level_menu():
-        mainmenu._open(level)
+    def player_vs_computer_game():
+        # Add your logic here for player vs computer mode
+        print("Not yet implemented")
+        main_menu._open(computer_mode_menu)
+        pass
 
+    """
     def set_difficulty(value, difficulty):
         print(value)
         print(difficulty)
+    """
+    def start_computer_game(difficulty):
+        # Set the game mode
+        game_mode = "computer"
+        # Start the game
+        game = Game(screen)
+        game.set_game_mode(game_mode)
+        game.play()
 
     def about_menu():
-        mainmenu._open(about)
+        main_menu._open(about)
 
     def host_game():
         mpgame = MPGame(screen)
         mpgame.host_game()
 
-
     def join_game():
         mpgame = MPGame(screen)
         mpgame.join_game()
 
+    # Menu screens
 
-    #Main menu screen
-    mainmenu = pygame_menu.Menu('WELCOME TO HEX', WIDTH, HEIGHT, theme=themes.THEME_DARK)
+    main_menu = pygame_menu.Menu('Welcome to Hex', WIDTH, HEIGHT, theme=themes.THEME_DARK)
+    game_mode_menu = pygame_menu.Menu('Select Game Mode', WIDTH, HEIGHT, theme=themes.THEME_DARK)
+    computer_mode_menu = pygame_menu.Menu('Select Difficulty', WIDTH, HEIGHT, theme=themes.THEME_DARK)
 
-    # The rest of the buttons
-    play_button = mainmenu.add.button('Play', start_game)
-    host_button = mainmenu.add.button('Host', host_game)
-    join_button = mainmenu.add.button('Join', join_game)
-    level_button = mainmenu.add.button('Computer Level', level_menu)
-    about_button = mainmenu.add.button('About the Game', about_menu)
-    quit_button = mainmenu.add.button('Quit', pygame_menu.events.EXIT)
+    # Main menu buttons
 
-    level = pygame_menu.Menu('Select a Difficulty', WIDTH, HEIGHT, theme=themes.THEME_DARK)
-    level.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2), ('Medium', 3)], onchange=set_difficulty)
+    play_button = main_menu.add.button('Play', start_game)
+    host_button = main_menu.add.button('Host', host_game)
+    join_button = main_menu.add.button('Join', join_game)
+    # level_button = main_menu.add.button('Computer Level', level_menu)
+    about_button = main_menu.add.button('About the Game', about_menu)
+    quit_button = main_menu.add.button('Quit', pygame_menu.events.EXIT)
+
+    # Game mode menu buttons
+
+    player_vs_player_button = game_mode_menu.add.button('Player vs Player', player_vs_player_game)
+    player_vs_computer_button = game_mode_menu.add.button('Player vs Computer', player_vs_computer_game)
+    back_button = game_mode_menu.add.button('Back', pygame_menu.events.BACK)
+
+    # Computer game menu buttons
+
+    easy_button = computer_mode_menu.add.button('Easy', lambda: start_computer_game('easy'))
+    medium_button = computer_mode_menu.add.button('Medium', lambda: player_vs_computer_game('medium'))
+    hard_button = computer_mode_menu.add.button('Hard', lambda: player_vs_computer_game('hard'))
+    back_button = computer_mode_menu.add.button('Back', pygame_menu.events.BACK)
+
+    # level = pygame_menu.Menu('Select a Difficulty', WIDTH, HEIGHT, theme=themes.THEME_DARK)
+    # level.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2), ('Medium', 3)], onchange=set_difficulty)
     arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size=(10, 15))
 
     about = pygame_menu.Menu('About Hex', WIDTH, HEIGHT, theme=themes.THEME_DARK)
@@ -76,17 +111,15 @@ def show_menu():
 
     join = pygame_menu.Menu('Join game', WIDTH, HEIGHT, theme=themes.THEME_DARK)
 
-
-
     while True:
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 exit()
-        if mainmenu.is_enabled():
-            mainmenu.update(events)
-            mainmenu.draw(surface)
-            if mainmenu.get_current().get_selected_widget():
-                arrow.draw(surface, mainmenu.get_current().get_selected_widget())
+        if main_menu.is_enabled():
+            main_menu.update(events)
+            main_menu.draw(surface)
+            if main_menu.get_current().get_selected_widget():
+                arrow.draw(surface, main_menu.get_current().get_selected_widget())
 
         pygame.display.update()
